ea569cc5f63e45db1cc7f2fc8a717e094a2debfb
Sachin Rathod <sr5631080@gmail.com>
2025-12-06 20:14:10 +0530

    py code

---
+++ fun.py
@@ -0,0 +1,9 @@
+#python list  
+#Lists are used to store multiple items in a single variable.
+myList = ["apple" , "banana"];
+print(myList[0])
+print(len(myList))
+print(type(myList))
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
---
+++ py-baisc.py
@@ -0,0 +1,20 @@
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+a = 'this is me sachin   ' 
+
+print(a.upper())
+print(a.strip())
+
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+print(a.split(","))  # returns ['Hello', ' World!']
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
