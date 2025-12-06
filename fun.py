02d35afc348f97b64036c5c81923fd174498fd04
Sachin Rathod <sr5631080@gmail.com>
2025-12-06 23:30:59 +0530

    aded

--- fun.py
+++ fun.py
@@ -1,9 +1,27 @@
 #python list  
 #Lists are used to store multiple items in a single variable.
-myList = ["apple" , "banana"];
-print(myList[0])
-print(len(myList))
-print(type(myList))
 
-thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# Dictionary is a collection which is ordered** and changeable. No duplicate members.
+# myList = ["apple" , "banana" , "cheery" , "mango"];
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[:-5])
+# print(myList[1 : 3])
+# print(myList[0])
+# print(myList[0])
+# print(len(myList))
+# print(type(myList))
+
+# thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+# print(thislist)
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(len(thislist  ))
+thislist.insert(2, "lemon")
 print(thislist)
+print(len(thislist  ))
