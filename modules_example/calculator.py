b16fe426dfbc5e41ec3e893556558e27ce32fdf4
Sachin Rathod <sr5631080@gmail.com>
2026-02-08 00:42:55 +0530

    aded

--- modules_example/calculator.py
+++ modules_example/calculator.py
@@ -1,5 +1,9 @@
 
 
-
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
 def aditionOfTwoDigit(x , y):
     return x+y
--- modules_example/call.py
+++ modules_example/call.py
@@ -1,5 +1,20 @@
-import calculator
+# import calculator
+# import datetime
 
-ans = calculator.aditionOfTwoDigit(12, 5)
+# x = datetime.datetime(2018, 6, 1)
+# print(x.strftime("%B"))
+import json
 
-print(ans)
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
+# ans = calculator.aditionOfTwoDigit(12, 5)
+# x = dir(calculator)
+# print(ans)
+# print(x)
+# print(calculator.person1["name"])
